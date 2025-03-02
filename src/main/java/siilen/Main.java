1abbb99dede1e98313fe58446b0bf8844426ed4f
Aamgk <osiilen@bk.ru>
2025-03-02 15:39:10 +0300

    the next two assignments(single number and contains duplicate)

--- src/main/java/siilen/Main.java
+++ src/main/java/siilen/Main.java
@@ -6,21 +6,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan  = new Scanner(System.in);
+
+        /*Palindrome Number*/
         System.out.println("Enter a number to check for a palindrome: ");
         int number = scan.nextInt();
-
-        System.out.println("Enter the size of the array: ");
-        int nums_length = scan.nextInt();
-
-        int[] nums = new int[nums_length];
-        for (int i = 0; i < nums_length; i++){
-            System.out.println("Enter the array element: ");
-            int num = scan.nextInt();
-            nums[i] = num;
-        }
-        System.out.println("Enter target: ");
-        int target = scan.nextInt();
-        scan.close();
 
         PalindromeNumber palindromeNumber = new PalindromeNumber();
         if (palindromeNumber.isPalindrome(number)) {
@@ -29,7 +18,47 @@ public class Main {
             System.out.println("No, the number "+ number + " not a palindrome.");
         }
 
+        /*Two Sums*/
+        System.out.println("Enter the size of the array: ");
+        int nums_length = scan.nextInt();
+
+        int[] nums = new int[nums_length];
+        for (int i = 0; i < nums_length; i++){
+            System.out.println("Enter the array elements: ");
+            int num = scan.nextInt();
+            nums[i] = num;
+        }
+        System.out.println("Enter target: ");
+        int target = scan.nextInt();
+
         TwoSums twoSums = new TwoSums();
         System.out.println(Arrays.toString(twoSums.twoSum(nums, target)));
+
+        /*Single Number*/
+        System.out.println("Enter the size of the array: ");
+        int single_nums_length = scan.nextInt();
+
+        int[] single_nums = new int[single_nums_length];
+        for (int i = 0; i < single_nums_length; i++){
+            System.out.println("Enter the array elements: ");
+            int Snum = scan.nextInt();
+            single_nums[i] = Snum;
+        }
+        SingleNumber singleNumber = new SingleNumber();
+        System.out.println("The single number: " + singleNumber.singleNumber(single_nums));
+
+        /*Contains Duplicate*/
+        System.out.println("Enter the size of the array: ");
+        int multi_nums_length = scan.nextInt();
+        int[] multi_nums = new int[multi_nums_length];
+        for (int i = 0; i < multi_nums_length; i++){
+            System.out.println("Enter the array elements: ");
+            int Mnum = scan.nextInt();
+            multi_nums[i] = Mnum;
+        }
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        System.out.println(containsDuplicate.containsDuplicate(multi_nums));
+
+        scan.close();
     }
 }
